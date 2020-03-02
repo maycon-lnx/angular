610799e39919c9296453799975dfb5963948467e
maycon-lnx <56367704+maycon-lnx@users.noreply.github.com>
2020-03-02 12:13:50 -0300

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -4,5 +4,6 @@ create table funcionario (ID_FUNC int(20), NOME varchar(255), SALARIO float(10,2
 create table depto (ID_DEPTO int(5), DESCRI varchar(255) PRIMARY KEY (ID_DEPTO));
 
                                                                       
-novo:                                                                      
+novo:   
+create table funcionario (ID_FUNC int(20), NOME varchar(255), SALARIO float(10,2), ID_DEPTO int(5), IDADE int(2),  PRIMARY KEY (ID_FUNC), FOREIGN KEY (ID_DEPTO) REFERENCES depto(ID_DEPTO));
 create table depto (DESCRI varchar(255), ID_DEPTO int auto_increment, PRIMARY KEY (ID_DEPTO));
